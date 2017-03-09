2a7c2847c1f009e27a57a5e65974be94fcc0911f
GeoffWacker <geoffwacker@gmail.com>
2017-03-09 14:08:17 -0800

    tinyfs empty functions

--- libTinyFS.c
+++ libTinyFS.c
@@ -1,5 +1,39 @@
 #include <stdio.h>
+#include "tinyFS_errno.h"
+#include "tinyFS.h"
 
-int main(int argc, char *argv[]) {
-   
+int tfs_mkfs(char *filename, int nBytes) {
+	return 0;
+}
+
+int tfs_mount(char *diskname) {
+	return 0;
+}
+
+int tfs_unmount(void) {
+	return 0;
+}
+
+fileDescriptor tfs_openFile(char *name) {
+	return 0;
+}
+
+int tfs_closeFile(fileDescriptor FD) {
+	return 0;
+}
+
+int tfs_writeFile(fileDescriptor FD,char *buffer, int size) {
+	return 0;
+}
+
+int tfs_deleteFile(fileDescriptor FD) {
+	return 0;
+}
+
+int tfs_readByte(fileDescriptor FD, char *buffer) {
+	return 0;
+}
+
+int tfs_seek(fileDescriptor FD, int offset) {
+	return 0;
 }
