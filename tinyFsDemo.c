a2de4417aa2e5f9544fa5819638c668ca857ee01
Justin Foxhoven <djustinf@gmail.com>
2017-03-16 13:45:16 -0700

    Demo created

--- tinyFsDemo.c
+++ tinyFsDemo.c
@@ -1,5 +1,112 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "tinyFS.h"
+#include "libTinyFS.h"
+#include "tinyFS_errno.h"
+
+void waitForEnter() {
+    printf("Press ENTER to proceed...\n");
+    getchar();
+}
 
 int main(int argc, char *argv[]) {
-   
+    char readBuffer;
+    char content1[] = "content 1";
+    char content2[] = "content 2";
+    char content3[] = "content 3";
+
+    fileDescriptor FD1, FD2, FD3;
+    waitForEnter();
+    printf("Attempting to mount disk\n");
+    if (tfs_mount(DEFAULT_DISK_NAME) < 0) {
+        tfs_mkfs(DEFAULT_DISK_NAME, DEFAULT_DISK_SIZE);
+        if (tfs_mount(DEFAULT_DISK_NAME) < 0) {
+            perror("disk mount failed, exiting...\n");
+            return -1;
+        }
+    }
+    printf("%s: mounted\n");
+    waitForEnter();
+    printf("Attempting to open files\n");
+    FD1 = tfs_openFile("File1");
+    if (FD1 < 0)
+        perror("failed to open File1");
+    else
+        printf("File1 opened\n");
+
+    FD2 = tfs_openFile("File2");
+    if (FD2 < 0)
+        perror("failed to open File2");
+    else
+        printf("File2 opened\n");
+    
+    FD1 = tfs_openFile("File3");
+    if (FD3 < 0)
+        perror("failed to open File3");
+    else
+        printf("File3 opened\n");
+
+    waitForEnter();
+    printf("Writing content to files\n");
+    if (tfs_writeFile(FD1, content1, 20) < 0)
+        perror("write to File1 failed");
+    else
+        printf("Wrote to File1\n");
+    
+    if (tfs_writeFile(FD2, content2, 20) < 0)
+        perror("write to File2 failed");
+    else
+        printf("Wrote to File2\n");
+
+    if (tfs_writeFile(FD3, content3, 20) < 0)
+        perror("write to File3 failed");
+    else
+        printf("Wrote to File3\n");
+
+    waitForEnter();
+    printf("Reading from File 1...\n");
+    while (tfs_readByte(FD1, &readBuffer) >= 0)
+        printf("%c", readBuffer);
+    printf("\n");
+    
+    waitForEnter();
+    printf("Files present:\n");
+    tfs_readdir();
+    
+    waitForEnter();
+    printf("Renaming File2 to Blah2\n");
+    if ((tfs_rename(FD2, "Blah2")) < 0)
+        perror("failed to rename File2");
+    printf("Files present:\n");
+    tfs_readdir();
+
+    waitForEnter();
+    printf("Closing File3, then attempting to rename to Blah3\n");
+    if (tfs_closeFile(FD3) < 0)
+        perror("Error closing File3");
+    if ((tfs_rename(FD3, "Blah3")) < 0)
+        perror("failed to rename File3");
+    printf("Files present:\n");
+    tfs_readdir();
+
+    /*
+        other tests here
+    */
+
+    waitForEnter();
+    printf("Deleting Blah2\n");
+    tfs_deleteFile(FD2);
+    printf("Files present:\n");
+    tfs_readdir();
+
+    waitForEnter();
+    printf("Unmounting: %s\n", DEFAULT_DISK_NAME);
+    if (tfs_unmount() < 0)
+        perror("Unmount failed");
+    else
+        printf("Unmount successful");
+    printf("\nEND OF DEMO\n");
+    return 0;
 }
